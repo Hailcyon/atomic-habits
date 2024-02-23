11c9efdc48eaa4b0647a633c535656cde9be9ae2
Emeier2 <U0670874@umail.utah.edu>
2024-02-23 09:46:36 -0700

    added local notification service

--- src-code/lib/service/notification_service.dart
+++ src-code/lib/service/notification_service.dart
@@ -1,30 +1,74 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:timezone/data/latest.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
+import '../main.dart';
+import '../presentation/log_in_screen/log_in_screen.dart';
 
 class NotificationService {
-  //Singleton pattern
+  // Singleton pattern
   static final NotificationService _notificationService =
       NotificationService._internal();
   factory NotificationService() {
     return _notificationService;
   }
+
   NotificationService._internal();
 
-  //instance of FlutterLocalNotificationsPlugin
+  static const channelId = "1";
+
   final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
       FlutterLocalNotificationsPlugin();
-  final AndroidInitializationSettings initializationSettingsAndroid =
-      AndroidInitializationSettings('app_icon');
-  //Initialization Settings for iOS devices
-  final DarwinInitializationSettings initializationSettingsIOS =
-      DarwinInitializationSettings(
-    requestSoundPermission: false,
-    requestBadgePermission: false,
-    requestAlertPermission: false,
+
+  static final AndroidNotificationDetails _androidNotificationDetails =
+      AndroidNotificationDetails(
+    channelId,
+    "thecodexhub",
+    channelDescription:
+        "This channel is responsible for all the local notifications",
+    playSound: true,
+    priority: Priority.high,
+    importance: Importance.high,
   );
 
-  void requestIOSPermissions(
-      FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin) {
-    flutterLocalNotificationsPlugin
+  static final DarwinNotificationDetails _DarwinNotificationDetails =
+      DarwinNotificationDetails();
+
+  final NotificationDetails notificationDetails = NotificationDetails(
+    android: _androidNotificationDetails,
+    iOS: _DarwinNotificationDetails,
+  );
+
+  Future<void> init() async {
+    final AndroidInitializationSettings androidInitializationSettings =
+        AndroidInitializationSettings("icon");
+
+    final DarwinInitializationSettings darwinInitializationSettings =
+        DarwinInitializationSettings(
+      defaultPresentAlert: false,
+      defaultPresentBadge: false,
+      defaultPresentSound: false,
+    );
+
+    final InitializationSettings initializationSettings =
+        InitializationSettings(
+      android: androidInitializationSettings,
+      iOS: darwinInitializationSettings,
+    );
+
+    // *** Initialize timezone here ***
+    tz.initializeTimeZones();
+
+    await flutterLocalNotificationsPlugin.initialize(
+      initializationSettings,
+      onDidReceiveNotificationResponse: (NotificationResponse response) {
+        // Handle notification response here
+      },
+    );
+  }
+
+  Future<void> requestIOSPermissions() async {
+    await flutterLocalNotificationsPlugin
         .resolvePlatformSpecificImplementation<
             IOSFlutterLocalNotificationsPlugin>()
         ?.requestPermissions(
@@ -34,27 +78,50 @@ class NotificationService {
         );
   }
 
-  Future<void> init() async {
-    //Initialization Settings for Android
-    final AndroidInitializationSettings initializationSettingsAndroid =
-        AndroidInitializationSettings('app_icon');
-
-    //Initialization Settings for iOS
-    final DarwinInitializationSettings initializationSettingsIOS =
-        DarwinInitializationSettings(
-      requestSoundPermission: false,
-      requestBadgePermission: false,
-      requestAlertPermission: false,
+  Future<void> showNotification(
+      int id, String title, String body, String payload) async {
+    await flutterLocalNotificationsPlugin.show(
+      id,
+      title,
+      body,
+      notificationDetails,
+      payload: payload,
     );
+  }
 
-    //InitializationSettings for initializing settings for both platforms (Android & iOS)
-    final InitializationSettings initializationSettings =
-        InitializationSettings(
-            android: initializationSettingsAndroid,
-            iOS: initializationSettingsIOS);
-
-    await flutterLocalNotificationsPlugin.initialize(
-      initializationSettings,
+  Future<void> scheduleNotification(int id, String title, String body,
+      DateTime eventDate, TimeOfDay eventTime, String payload,
+      [DateTimeComponents? dateTimeComponents]) async {
+    final scheduledTime = eventDate.add(Duration(
+      hours: eventTime.hour,
+      minutes: eventTime.minute,
+    ));
+    await flutterLocalNotificationsPlugin.zonedSchedule(
+      id,
+      title,
+      body,
+      tz.TZDateTime.from(scheduledTime, tz.local),
+      notificationDetails,
+      uiLocalNotificationDateInterpretation:
+          UILocalNotificationDateInterpretation.absoluteTime,
+      androidAllowWhileIdle: true,
+      payload: payload,
+      matchDateTimeComponents: dateTimeComponents,
     );
+  }
+
+  Future<void> cancelNotification(int id) async {
+    await flutterLocalNotificationsPlugin.cancel(id);
+  }
+
+  Future<void> cancelAllNotifications() async {
+    await flutterLocalNotificationsPlugin.cancelAll();
+  }
+}
+
+Future<void> onSelectNotification(String? payload) async {
+  Future<void> navigateToLoginPage(BuildContext context) async {
+    await Navigator.of(context)
+        .push(MaterialPageRoute(builder: (_) => LoginPage()));
   }
 }
