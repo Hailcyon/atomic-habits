1848989d018530467f5b7b0e67055c755ff0e1f3
Emeier2 <U0670874@umail.utah.edu>
2024-03-22 08:06:28 -0600

    added save features

--- src-code/lib/presentation/law_two_page/tempt_bundle.dart
+++ src-code/lib/presentation/law_two_page/tempt_bundle.dart
@@ -2,13 +2,18 @@ import 'package:ahapp3/core/utils/size_utils.dart';
 import 'package:ahapp3/routes/app_routes.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:ahapp3/service/database.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class TemptBundle extends StatefulWidget {
   // const ReduceFrictionPage({Key? key}) : super(key: key);
   final Function(String) onSave;
 
-  const TemptBundle({Key? key, required this.onSave}) : super(key: key);
+  final String habitId;
 
+  const TemptBundle({Key? key, required this.onSave, required this.habitId})
+      : super(key: key);
 
   @override
   State<TemptBundle> createState() => _TemptBundle();
@@ -22,6 +27,10 @@ class _TemptBundle extends State<TemptBundle> {
     "Suggestion 3",
     // Add more suggestions as needed
   ];
+
+  final DatabaseService dbService = DatabaseService(
+      uid: FirebaseAuth.instance.currentUser?.uid ??
+          ''); // Initialize dbService with the user's UID;
 
   @override
   Widget build(BuildContext context) {
@@ -45,7 +54,7 @@ class _TemptBundle extends State<TemptBundle> {
     );
   }
 
-  Widget _reduceFrictionDetail(BuildContext context){
+  Widget _reduceFrictionDetail(BuildContext context) {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 16),
       child: Column(
@@ -75,6 +84,7 @@ class _TemptBundle extends State<TemptBundle> {
           SizedBox(height: 16.0),
           MaterialButton(
             onPressed: () {
+              saveHabitLaw(context, userInputController.text);
               //widget.onSave("I will make going for a run easier by ${userInputController.text}");
             },
             color: Color.fromARGB(255, 1, 82, 148),
@@ -95,47 +105,78 @@ class _TemptBundle extends State<TemptBundle> {
     );
   }
 
-  Widget _reduceFrictionSugguestion(BuildContext context){
+  Widget _reduceFrictionSugguestion(BuildContext context) {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 16),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Padding(
-          padding: EdgeInsets.only(bottom:0), // Space between the label and the first suggestion
-          child: Text(
-            "Suggestions:", // The label text
-            style: TextStyle(
-              color: Colors.black, // Color of the label
-              fontSize: 20, // Size of the label text
-              // fontWeight: FontWeight.bold, // Bold text for the label
-            ),
-          ),
-        ),
-        ...suggestions.map((suggestion) => InkWell(
-          onTap: () {
-            setState(() {
-              userInputController.text = suggestion;
-            });
-          },
-          child: Container(
-            margin: EdgeInsets.only(top: 20),
-            padding: EdgeInsets.all(10),
-            color: Color.fromARGB(255, 237, 207, 116),
+            padding: EdgeInsets.only(
+                bottom: 0), // Space between the label and the first suggestion
             child: Text(
-              suggestion,
+              "Suggestions:", // The label text
               style: TextStyle(
-                color: Colors.black,
-                fontSize: 16,
+                color: Colors.black, // Color of the label
+                fontSize: 20, // Size of the label text
+                // fontWeight: FontWeight.bold, // Bold text for the label
               ),
-              // softWrap: true, // Allow text wrapping
-              // overflow: TextOverflow.visible, // Show all text
             ),
           ),
-        )).toList(),
+          ...suggestions
+              .map((suggestion) => InkWell(
+                    onTap: () {
+                      setState(() {
+                        userInputController.text = suggestion;
+                      });
+                    },
+                    child: Container(
+                      margin: EdgeInsets.only(top: 20),
+                      padding: EdgeInsets.all(10),
+                      color: Color.fromARGB(255, 237, 207, 116),
+                      child: Text(
+                        suggestion,
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                        ),
+                        // softWrap: true, // Allow text wrapping
+                        // overflow: TextOverflow.visible, // Show all text
+                      ),
+                    ),
+                  ))
+              .toList(),
         ],
       ),
     );
   }
 
+  void saveHabitLaw(BuildContext context, String habitStr) async {
+    //habitNum = habit law (1 2 3 or 4)
+    final int habitNum = 1;
+
+    //habitLawNum = name of habit law
+    final String habitLawNum = "Environment Design";
+
+    if (habitStr.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Input is empty.")),
+      );
+    } else {
+      final FirebaseAuth auth = FirebaseAuth.instance;
+      final String uid = auth.currentUser?.uid ?? '';
+
+      //habitStr Should be input controller.text
+      String fin_habitStr = "Envrionment design:\n" + habitStr;
+
+      // Save the habit and update the ID with the one generated by Firestore
+      await dbService.saveHabitLaw(
+          widget.habitId, habitNum, habitLawNum, fin_habitStr);
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text("Habit law has been added!")));
+
+      // Navigator.of(context).pushNamed(AppRoutes.editHabitPageRoute, arguments: widget.habitId);
+      // Navigator.popUntil(context, ModalRoute.withName(AppRoutes.homePageRoute));
+    }
+  }
 }
